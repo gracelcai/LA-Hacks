529d9809fa3c16c3df3154596a9ba796bc611c93
Grace Cai <grace.cai7@gmail.com>
2021-04-24 16:11:50 -0700

    empty screens

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:la_hacks/screens/authenticate/authenticate.dart';
+import 'package:la_hacks/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return home or authenticate widget
+    return Authenticate();
+  }
+}
